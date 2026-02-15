6e52808d8d83387ef5d4833379bb99fd47ad7324
Jack Werner <jackawerner+github@gmail.com>
2026-02-14 17:55:08 -0800

    started work on new piece functions for attack boards

--- src/piece/piece.cpp
+++ src/piece/piece.cpp
@@ -1,7 +1,9 @@
 #include "Piece.hpp"
 
+#include <algorithm>
 #include <cstdint>
 #include <iostream>
+#include <vector>
 
 std::pair<bool, sb> Piece::move(const fb &bBoard, const sb &currentPos, const sb &futurePos) {
 	const int bIndex = getBoardIndex(bBoard, currentPos);
@@ -13,7 +15,7 @@ std::pair<bool, sb> Piece::move(const fb &bBoard, const sb &currentPos, const sb
 
 	if (const sb validMoves = getValidMoves(bBoard, currentPos); (futurePos & validMoves) == 0) { return {false, {}}; }
 
-	return {true, bBoard[bIndex] & ~currentPos | futurePos};
+	return {true, (bBoard[bIndex] & ~currentPos) | futurePos};
 }
 
 sb Piece::getValidMoves(const fb &bBoard, const sb &currentPos) {
@@ -25,32 +27,36 @@ sb Piece::getValidMoves(const fb &bBoard, const sb &currentPos) {
 
 	switch (bIndex / 2) {
 		case 0: {
-			// pawn
 			sb tempBoard = bIndex % 2 == 0 ? currentPos << 8 : currentPos >> 8;
 
 			const sb sColorBoard = getColorBoard(bBoard, bIndex % 2);
 			const sb dColorBoard = getColorBoard(bBoard, !(bIndex % 2));
 
 			// take left
-			if ((dColorBoard & tempBoard) << 1 != 0) { validMoves |= (tempBoard << 1); }
+			if ((dColorBoard & (tempBoard << 1)) != 0) { validMoves |= (tempBoard << 1); }
 
 			// take right
-			if ((dColorBoard & tempBoard) >> 1 != 0) { validMoves |= (tempBoard >> 1); }
+			if ((dColorBoard & (tempBoard >> 1)) != 0) { validMoves |= (tempBoard >> 1); }
 
 			// move forward
 			if ((tempBoard & (sColorBoard | dColorBoard)) == 0) {
 				validMoves |= tempBoard;
 
-				// move forward twice?
+				// move forward twice
 				tempBoard = bIndex % 2 == 0 ? tempBoard << 8 : tempBoard >> 8;
-				if (bIndex % 2 && (currentPos & 0x00FF00000000000000ULL) != 0 || !(bIndex % 2) && (
-					    currentPos & 0x00000000000000FF00ULL) != 0) { validMoves |= tempBoard; }
+				if ((bIndex % 2 && (currentPos & 0x00FF000000000000ULL) != 0) ||
+				    (!(bIndex % 2) && (currentPos & 0x000000000000FF00ULL) != 0)) { validMoves |= tempBoard; }
 			}
 			break;
-		}
+		} // pawn
 		case 1: {
-			// bishop
-			auto [edges, boundaries] = edgeCheck(currentPos);
+			constexpr std::array<sb, 4> bEdges{
+				0xFF00000000000000ULL, // top
+				0x0101010101010101ULL, // right
+				0x00000000000000FFULL, // bottom
+				0x8080808080808080ULL // left
+			};
+			auto [edges, boundaries] = edgeCheck(currentPos, bEdges);
 
 			uint8_t validDirections{0xF}; // MSB is tl, tr, br, bl
 
@@ -77,19 +83,86 @@ sb Piece::getValidMoves(const fb &bBoard, const sb &currentPos) {
 			for (int i = 0; i < 2; i++) {
 				for (int j = 0; j < 2; j++) {
 					if ((validDirections & (0x8 >> ((i * 2) + j))) != 0) {
-						constexpr std::array<int, 4> directions{9, 7};
+						constexpr std::array<int, 2> directions{9, 7};
 						validMoves |= getDirection(bBoard, boundaries, currentPos, directions[j], i == 0, bIndex % 2);
 					}
 				}
 			}
 			break;
-		}
+		} // bishop
 		case 2: {
-			// knight
-		}
+			constexpr std::array<sb, 4> iBEdges{
+				0xFFFF000000000000ULL, // top
+				0x0303030303030303ULL, // right
+				0x000000000000FFFFULL, // bottom
+				0xC0C0C0C0C0C0C0C0ULL // left
+			};
+
+			auto [iEdges, iBoundaries] = edgeCheck(currentPos, iBEdges);
+
+			constexpr std::array<sb, 4> oBEdges{
+				0xFF00000000000000ULL, // top
+				0x0101010101010101ULL, // right
+				0x00000000000000FFULL, // bottom
+				0x8080808080808080ULL // left
+			};
+
+			auto [oEdges, oBoundaries] = edgeCheck(currentPos, oBEdges);
+
+			uint8_t validDirections{0xFF}; // MSB is lr, ll, tr, tl, lll, llr, bl, br
+
+			// top edge
+			if ((0x8 & oEdges) == 0) {
+				validDirections &= ~0xF0; // lr ll tr tl
+			} else if ((0x8 & iEdges) == 0) {
+				validDirections &= ~0x30; // tr tl
+			}
+
+			// right edge
+			if ((0x4 & oEdges) == 0) {
+				validDirections &= ~0xA5; // lr tr llr br
+			} else if ((0x4 & iEdges) == 0) {
+				validDirections &= ~0x84; // lr, llr
+			}
+
+			// bottom edge
+			if ((0x2 & oEdges) == 0) {
+				validDirections &= ~0xF; // lll, llr, bl, br
+			} else if ((0x2 & iEdges) == 0) {
+				validDirections &= ~0x3; // bl, br
+			}
+
+			// left edge
+			if ((0x1 & oEdges) == 0) {
+				validDirections &= ~0x5A; // ll, tl, lll, bl
+			} else if ((0x1 & iEdges) == 0) {
+				validDirections &= ~0x48; // ll, lll
+			}
+
+			const sb sColorBoard{getColorBoard(bBoard, (bIndex % 2))};
+
+			for (int i = 0; i < 2; i++) {
+				for (int j = 0; j < 4; j++) {
+					if ((validDirections & (0x80 >> ((i * 4) + j))) != 0) {
+						constexpr std::array<int, 4> directions{6, 10, 15, 17};
+
+						if (const sb tempBoard = i == 0 ? currentPos << directions[j] : currentPos >> directions[j];
+							(tempBoard & sColorBoard) == 0) { validMoves |= tempBoard; }
+					}
+				}
+			}
+
+			break;
+		} // knight
 		case 3: {
-			// rook
-			auto [edges, boundaries] = edgeCheck(currentPos);
+			constexpr std::array<sb, 4> bEdges{
+				0xFF00000000000000ULL, // top
+				0x0101010101010101ULL, // right
+				0x00000000000000FFULL, // bottom
+				0x8080808080808080ULL // left
+			};
+
+			auto [edges, boundaries] = edgeCheck(currentPos, bEdges);
 
 			uint8_t validDirections{0xF}; // MSB is l, t, r, b
 
@@ -116,16 +189,22 @@ sb Piece::getValidMoves(const fb &bBoard, const sb &currentPos) {
 			for (int i = 0; i < 2; i++) {
 				for (int j = 0; j < 2; j++) {
 					if ((validDirections & (0x8 >> ((i * 2) + j))) != 0) {
-						constexpr std::array<int, 4> directions{1, 8};
+						constexpr std::array<int, 2> directions{1, 8};
 						validMoves |= getDirection(bBoard, boundaries, currentPos, directions[j], i == 0, bIndex % 2);
 					}
 				}
 			}
 			break;
-		}
+		} // rook
 		case 4: {
-			// queen
-			auto [edges, boundaries] = edgeCheck(currentPos);
+			constexpr std::array<sb, 4> bEdges{
+				0xFF00000000000000ULL, // top
+				0x0101010101010101ULL, // right
+				0x00000000000000FFULL, // bottom
+				0x8080808080808080ULL // left
+			};
+
+			auto [edges, boundaries] = edgeCheck(currentPos, bEdges);
 
 			uint8_t validDirections{0xFF}; // MSB is l, tl, t, tr, r, br, b, bl
 
@@ -159,8 +238,54 @@ sb Piece::getValidMoves(const fb &bBoard, const sb &currentPos) {
 			}
 
 			break;
-		}
-		case 5: // king
+		} // queen
+		case 5: {
+			constexpr std::array<sb, 4> bEdges{
+				0xFF00000000000000ULL, // top
+				0x0101010101010101ULL, // right
+				0x00000000000000FFULL, // bottom
+				0x8080808080808080ULL // left
+			};
+
+			auto [edges, boundaries] = edgeCheck(currentPos, bEdges);
+
+			uint8_t validDirections{0xFF}; // MSB is l, tl, t, tr, r, br, b, bl
+
+			// top edge
+			if ((0x8 & edges) == 0) {
+				validDirections &= ~0x70; // tl, t, tr
+			}
+
+			// right edge
+			if ((0x4 & edges) == 0) {
+				validDirections &= ~0x1C; // tr, r, br
+			}
+
+			// bottom edge
+			if ((0x2 & edges) == 0) {
+				validDirections &= ~0x7; // br, b, bl
+			}
+
+			// left edge
+			if ((0x1 & edges) == 0) {
+				validDirections &= ~0xC1; // bl, l, tl
+			}
+
+			const sb sColorBoard{getColorBoard(bBoard, (bIndex % 2))};
+
+			for (int i = 0; i < 2; i++) {
+				for (int j = 0; j < 4; j++) {
+					if ((validDirections & (0x80 >> ((i * 4) + j))) != 0) {
+						constexpr std::array<int, 4> directions{1, 7, 8, 9};
+
+						if (const sb tempBoard = i == 0 ? currentPos << directions[j] : currentPos >> directions[j];
+							(tempBoard & sColorBoard) == 0) { validMoves |= tempBoard; }
+					}
+				}
+			}
+
+			break;
+		} // king
 		default:
 			break;
 	}
@@ -174,19 +299,12 @@ int Piece::getBoardIndex(const fb &bBoard, const sb &currentPos) {
 	return -1;
 }
 
-std::pair<uint8_t, sb> Piece::edgeCheck(const sb &position) {\
+std::pair<uint8_t, sb> Piece::edgeCheck(const sb &position, const std::array<sb, 4> &bEdges) {\
 	uint8_t edges{0};
 	sb boundaries{0};
 
 	// each bit represents each of the sides
-
 	for (int i = 0; i < 4; i++) {
-		constexpr std::array<sb, 4> bEdges{
-			0xFF00000000000000ULL, // top
-			0x0101010101010101ULL, // right
-			0x00000000000000FFULL, // bottom
-			0x8080808080808080ULL // left
-		};
 		if ((bEdges[i] & position) == 0) {
 			edges |= (static_cast<uint8_t>(0x8) >> i);
 			boundaries |= bEdges[i];
@@ -223,4 +341,81 @@ sb Piece::getColorBoard(const fb &bBoard, const bool &color) {
 	for (int i = color; i < 12; i += 2) { outBoard |= bBoard[i]; }
 
 	return outBoard;
+}
+
+int Piece::getPieceIndexFromPosition(const pb &dPieces, const sb &currentPos) {
+	for (int i = 0; i < 16; i++) { if ((dPieces[i].position & currentPos) != 0) { return i; } }
+
+	return -1;
+}
+
+int Piece::getPieceIndexFromId(const pb &dPieces, const uint8_t &id) {
+	for (int i = 0; i < 16; i++) { if (dPieces[i].id == id) { return i; } }
+
+	return -1;
+}
+
+int Piece::boardToInt(sb board) {
+	int i = 0;
+
+	while (board > 0x1ULL) {
+		board >>= 1;
+		i++;
+	}
+
+	return i;
+}
+
+void Piece::updatePieceData(const fb &bBoard, sb &wAttackBoard, sb &bAttackBoard,
+                            pb &wPieces, pb &bPieces,
+                            ob &whiteObervers, ob &blackObservers,
+                            const sb &prevPos, const sb &newPos) {
+	const int bIndex = getBoardIndex(bBoard, prevPos);
+
+	if (bIndex == -1) { std::cerr << "Invalid position sent to getValidMoves" << std::endl; }
+
+	const bool color = bIndex % 2 == 0;
+
+	pb &sColorPieces = color ? wPieces : bPieces;
+	ob &sColorObservers = color ? whiteObervers : blackObservers;
+	const int pIndex = getPieceIndexFromPosition(sColorPieces, prevPos);
+
+	if (sColorPieces[pIndex].isSlider) {
+		for (auto &vec: sColorObservers) {
+			vec.erase(
+				std::remove(vec.begin(), vec.end(), sColorPieces[pIndex].id), vec.end()
+			);
+		}
+	}
+
+	sColorPieces[pIndex].attacks = getAttacks(bBoard, newPos, true);
+	sColorPieces[pIndex].position = newPos;
+
+	const int prevIndex = boardToInt(prevPos);
+	std::vector<uint8_t> idBuffer = whiteObervers[prevIndex];
+
+	for (uint8_t id: idBuffer) {
+		const int index = getPieceIndexFromId(wPieces, id);
+		wPieces[index].attacks = getAttacks(bBoard, wPieces[index].position, false);
+	}
+	idBuffer = blackObservers[prevIndex];
+
+	for (uint8_t id: idBuffer) {
+		const int index = getPieceIndexFromId(bPieces, id);
+		bPieces[index].attacks = getAttacks(bBoard, bPieces[index].position, false);
+	}
+
+	const int newIndex = boardToInt(newPos);
+	idBuffer = blackObservers[newIndex];
+
+	for (uint8_t id: idBuffer) {
+		const int index = getPieceIndexFromId(bPieces, id);
+		bPieces[index].attacks = getAttacks(bBoard, bPieces[index].position, false);
+	}
+	idBuffer = whiteObervers[newIndex];
+
+	for (uint8_t id: idBuffer) {
+		const int index = getPieceIndexFromId(wPieces, id);
+		wPieces[index].attacks = getAttacks(bBoard, wPieces[index].position, false);
+	}
 }
--- src/piece/piece.hpp
+++ src/piece/piece.hpp
@@ -2,17 +2,42 @@
 
 #include <array>
 #include <cstdint>
+#include <vector>
 
 using sb = uint64_t;
 using fb = std::array<sb, 12>;
+
+struct PieceData {
+	sb attacks;
+	sb position;
+	uint8_t boardIndex;
+	uint8_t id;
+	bool isSlider;
+};
+
+using pb = std::array<PieceData, 16>;
+using ob = std::array<std::vector<uint8_t>, 64>;
 
 class Piece {
 public:
 	static std::pair<bool, sb> move(const fb &bBoard, const sb &currentPos, const sb &futurePos);
 	static sb getValidMoves(const fb &bBoard, const sb &currentPos);
-	static int getBoardIndex(const fb &bBoard, const sb &currentPos);
-	static std::pair<uint8_t, sb> edgeCheck(const sb &position);
 	static sb getDirection(const fb &bBoard, const sb &boundaries, const sb &currentPos, const int &shift,
 	                       const bool &direction, const bool &color);
+
+	static std::pair<uint8_t, sb> edgeCheck(const sb &position, const std::array<sb, 4> &bEdges);
+	static int getBoardIndex(const fb &bBoard, const sb &currentPos);
 	static sb getColorBoard(const fb &bBoard, const bool &color);
+
+	static int getPieceIndexFromPosition(const pb &dPieces, const sb &currentPos)
+	static int getPieceIndexFromId(const pb &dPieces, const uint8_t &id);
+	static int boardToInt(sb board);
+	static void updatePieceData(const fb &bBoard, sb &wAttackBoard, sb &bAttackBoard,
+	                            pb &wPieces, pb &bPieces,
+	                            ob &whiteObervers, ob &blackObservers,
+	                            const sb &prevPos, const sb &newPos);
+	static void updateAttackBoards();
+	static sb getAttacks(const fb &bBoard, const sb &currentPos, const bool &isObservingNew);
+	static sb getAttackDirection(const fb &bBoard, const sb &boundaries, const sb &currentPos, const int &shift,
+	                             const bool &direction, const bool &color);
 };
